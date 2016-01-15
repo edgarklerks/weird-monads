313bce7b704ef720882044d89d3326f36d679c11
edgar.klerks <edgar.klerks@sanoma.com>
2016-01-15 10:09:18 +0100

    cleaned up StreamPipe a bit

--- src/Control/Monad/StreamPipe.hs
+++ src/Control/Monad/StreamPipe.hs
@@ -9,29 +9,6 @@ data Pipe i o a where
      Yield :: (() -> (o, Pipe i o a)) -> Pipe i o a
      Pure :: a -> Pipe i o a
 
-newtype InputPipe i e o = InputPipe {
-                  runInputPipe :: Pipe i o e
-          }
-
-instance Functor (InputPipe i e) where
-    fmap f m = InputPipe $ fmap' f (runInputPipe m)
-        where fmap' f (Pure a) = Pure a
-              fmap' q (Input f) = Input $ \i ->  runInputPipe $fmap q $ InputPipe (f i)
-              fmap' f (Yield g) = Yield $ \i -> let (o, p) = g i
-                                                in (f o, runInputPipe $ fmap f (InputPipe p))
-
-instance Applicative (InputPipe i e) where
-         pure a = InputPipe $ Yield (\() -> (a, runInputPipe $ pure a))
-         (<*>) f g = InputPipe $ worker (runInputPipe f) (runInputPipe g)
-                where worker (Input f) (Input g) = Input $ \i -> runInputPipe $ InputPipe ( f i) <*> InputPipe ( g i)
-                      worker _ (Pure g) =  Pure g
-                      worker (Pure a) _ = Pure a
-                      worker (Yield f)  (Yield g) = Yield $ \i -> let (f', pf') = f i
-                                                                      (a', pf'') = g i
-                                                                  in (f' a', runInputPipe $ InputPipe pf' <*> InputPipe pf'')
-                      worker (Input f) q = Input $ \i -> runInputPipe $ InputPipe (f i) <*> InputPipe q
-                      worker q (Input g) =  Input $ \i -> runInputPipe $ InputPipe q <*>  InputPipe (g i)
-
 instance Show (Pipe i o a) where
          show (Input _) = "Input state"
          show (Yield _) = "Yield state"
@@ -57,7 +34,7 @@ instance Monad (Pipe i o) where
          (>>=) (Input g) f = Input $ \i -> let (p) = g i
                                                       in (p >>= f)
 
-
+-- | Done one step of the computation
 stepPipe :: Pipe i o a -> i -> Either a (Maybe o, Pipe i o a)
 stepPipe (Yield g) _ = Right $ (let (o,p) = g () in (Just o, p)
                                           )
@@ -69,8 +46,8 @@ data PipeState i o a = Partial (Pipe i o a)
                      | Output ([i], o, Pipe i o a)
      deriving (Show)
 
-
--- stepTillOutput :: Pipe i o a -> [i] -> ([i], Either a (o, Pipe i o a))
+-- | Step till the first output
+stepTillOutput :: Pipe i o a -> [i] -> PipeState i o a
 stepTillOutput (Pure a) xs = Evaluated (xs, a)
 stepTillOutput (Yield g) xs = let (o, p) = g ()
                               in  Output (xs, o, p)
@@ -78,6 +55,7 @@ stepTillOutput t@(Input f) [] = Partial t
 stepTillOutput (Input f) (x:xs) = let p = f x
                                   in stepTillOutput p xs
 
+-- | Steps through the pipe till the input is empty or the pipe ends
 runPipe'' :: Pipe i o a -> [i] -> ([o], Maybe a)
 runPipe'' (Pure a)  _ = ([], Just a)
 runPipe'' (Yield g)  (xs) = let (o, p) = g ()
@@ -90,14 +68,18 @@ runPipe'' (Input f)  (x:xs) = let p = f x
 
 
 
+-- | `runPipe pipe n is`
+-- Run a pipe while n > 0, assumes length is >= n
 runPipe' :: Pipe i o a -> Int -> [i] -> ([o], Maybe a)
-runPipe' (Pure a) n xs = ([], Just a)
+rynPipe' _ 0 _ = ([], Nothing)
+runPipe' (Pure a) _ _ = ([], Just a)
 runPipe' (Yield g) n (xs) = let (o, p) = g ()
                                 (rs, a) = runPipe' p (n - 1) xs
                             in (o:rs, a)
 runPipe' (Input f) n (x:xs) = let p = f x
                               in runPipe' p (n - 1) xs
 
+-- | Run a pipe while continously consuming input, but not always use it in the computation.
 
 runPipe :: Pipe i o a -> [i] -> ([o], Maybe a)
 runPipe p [] = ([], Nothing)
@@ -109,59 +91,45 @@ runPipe p (i:is) = let res = stepPipe p i
                         Right (Nothing, p') -> let (xs, a) = runPipe p' is
                                                in (xs, a)
 
+-- | Read from the outer context
 input :: Pipe i o i
 input = Input $ \i -> (Pure i)
 
+-- | Give control back to the outer context. Sends an argument with it.
 yield :: o -> Pipe i o ()
 yield o = Yield $ \i -> (o, Pure ())
 
--- feed :: Pipe i j e -> Pipe j k e -> Pipe i k e
-feed p q = do
-     input >>= p >>= yield >>= q
-
 -- | Find tuples of odd naturals with the property:
 -- x^2 % y == x
+pairFinder :: [Integer] -> [(Integer, Integer)]
 pairFinder xs = fst $ runPipe'' pairFinder' $ join [[x,y]| x <- xs, y <- xs]
            where pairFinder' = forever $ do
                     x <- input
                     y <- input
                     when ( ((x * x) `mod` y == x)) $ yield (x,y)
 
-gen = forever $ yield 1
+-- | Hello world of generators
+gen1 :: Pipe t Int s
+gen1 = forever $ yield 1
+newtype InputPipe i e o = InputPipe {
+                  runInputPipe :: Pipe i o e
+          }
 
-newtype HistoryState s a = HistoryState {
-        runHistoryState :: [s] -> ([s], a)
-   }
+instance Functor (InputPipe i e) where
+    fmap f m = InputPipe $ fmap' f (runInputPipe m)
+        where fmap' f (Pure a) = Pure a
+              fmap' q (Input f) = Input $ \i ->  runInputPipe $fmap q $ InputPipe (f i)
+              fmap' f (Yield g) = Yield $ \i -> let (o, p) = g i
+                                                in (f o, runInputPipe $ fmap f (InputPipe p))
 
-instance Functor (HistoryState s) where
-         fmap f m = HistoryState $ \s -> let (s',a) = runHistoryState m s
-                                         in (s', f a)
-
-instance Applicative (HistoryState s) where
-         pure = return
-         (<*>) = ap
-
-instance Monad (HistoryState s) where
-         return a = HistoryState $ \s -> (s, a)
-         (>>=) m f = HistoryState $ \s -> let (s', a) = runHistoryState m s
-                                              (s'', b) = runHistoryState (f a) s'
-                                    in (s'', b)
-
-
-get :: Int -> HistoryState s [s]
-get n = HistoryState $ \s -> (s, take n s)
-
-set :: s -> HistoryState s ()
-set x = HistoryState $ \s -> (x:s, ())
-
-prune :: Int -> HistoryState s ()
-prune n = HistoryState $ \s -> (reverse . drop n . reverse $ s , ())
-
-goback :: Int -> HistoryState s ()
-goback n = HistoryState $ \s -> (drop n s, ())
-
-test = runHistoryState $ do
-     pp <- get 10
-     forM_ (pp `zip` [1..]) $ \(p,i) -> set (p * i)
-     goback 5
-     forM_ (pp `zip` [1..]) $ \(p,i) -> set (p + i)
+instance Applicative (InputPipe i e) where
+         pure a = InputPipe $ Yield (\() -> (a, runInputPipe $ pure a))
+         (<*>) f g = InputPipe $ worker (runInputPipe f) (runInputPipe g)
+                where worker (Input f) (Input g) = Input $ \i -> runInputPipe $ InputPipe ( f i) <*> InputPipe ( g i)
+                      worker _ (Pure g) =  Pure g
+                      worker (Pure a) _ = Pure a
+                      worker (Yield f)  (Yield g) = Yield $ \i -> let (f', pf') = f i
+                                                                      (a', pf'') = g i
+                                                                  in (f' a', runInputPipe $ InputPipe pf' <*> InputPipe pf'')
+                      worker (Input f) q = Input $ \i -> runInputPipe $ InputPipe (f i) <*> InputPipe q
+                      worker q (Input g) =  Input $ \i -> runInputPipe $ InputPipe q <*>  InputPipe (g i)
